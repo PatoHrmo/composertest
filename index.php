07619f5356fdab17409ce1f384affc02757f28f7
Patrik Hrmo <patohrmo@gmail.com>
2016-09-21 20:05:09 +0200

    pridane config

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "user";
+$config['db']['pass']   = "password";
+$config['db']['dbname'] = "exampleapp";
+
+
 require './vendor/autoload.php';
 
-$app = new \Slim\App;
+$app = new \Slim\App(["settings" => $config]);
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
